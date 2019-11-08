863a57d982daecd83175489816f0678d91fea4f7
boonaza <boonaza@gmail.com>
2019-11-08 10:29:58 -0500

    Added A system for Combat Animations and Yielding to Completion, Created global Cooldown status "Daze" that currently only applies to Flee

--- Global.gd
+++ Global.gd
@@ -5,6 +5,7 @@ extends Node
 # var b = "text"
 var EscapeChance = 80
 var Gold = 0
+var Ready = 1
 
 var Player_Position = Vector2(-125,-65)
 var Enemy_Position = Vector2(0,0)
@@ -13,6 +14,16 @@ func Store_Position(var PlrPos,var NmyPos):
 	Player_Position = -(PlrPos)
 	Enemy_Position = NmyPos
 
+func daze(var NumTurns):
+	Ready = 0
+	var t = Timer.new()
+	t.set_wait_time(NumTurns)
+	t.set_one_shot(true)
+	self.add_child(t)
+	t.start()
+	yield(t, "timeout")
+	Ready = 1
+	t.queue_free()
 
 # Called when the node enters the scene tree for the first time.
 
