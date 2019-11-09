313ccd41fd79a7ebf85c0877ed1d7b1eb40e9a13
boonaza <boonaza@gmail.com>
2019-11-08 19:20:36 -0500

    initialization

--- scripts/PlayerCMB.gd
+++ scripts/PlayerCMB.gd
@@ -1,7 +1,6 @@
-extends KinematicBody2D
+extends "PlayerOverworld.gd"
 
-const MOTION_SPEED = 50 # Pixels/second
-const initial_pos = Vector2(340, 360)
+
 var Def = PlayerVars.Defense
 var THP = PlayerVars.Health
 
@@ -12,18 +11,3 @@ func damage(var DMG):
 	if (THP <= 0):
 		PlayerVars.changeHP(1)
 
-func _physics_process(_delta):
-	var motion = Vector2()
-	
-	if Input.is_action_pressed("north"):
-		motion += Vector2(0, -1)
-	if Input.is_action_pressed("south"):
-		motion += Vector2(0, 1)
-	if Input.is_action_pressed("west"):
-		motion += Vector2(-1, 0)
-	if Input.is_action_pressed("east"):
-		motion += Vector2(1, 0)
-	
-	motion = motion.normalized() * MOTION_SPEED
-
-	move_and_slide(motion)
