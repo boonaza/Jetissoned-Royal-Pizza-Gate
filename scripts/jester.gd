248eed7e50d62f2edc919638242d647b2799a8dd
Hayden <hcoffey1@vols.utk.edu>
2019-11-09 00:59:24 -0500

    [dev/hc/main] Cleaned up sprite

--- scripts/jester.gd
+++ scripts/jester.gd
@@ -1,9 +1,5 @@
 extends "res://scripts/Movement.gd"
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	IDLE = "Idle"
@@ -13,7 +9,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-#	pass
 	if Input.is_action_pressed(ActWest):
 		get_node("Sprite").flip_h = true	
 	if Input.is_action_pressed(ActEast):
