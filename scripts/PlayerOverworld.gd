d9fdf47eadd5dda48416cc277b56e7b685371205
Hayden <hcoffey1@vols.utk.edu>
2019-11-15 16:16:04 -0500

    [dev/hc/main] Branch patch

--- scripts/PlayerOverworld.gd
+++ scripts/PlayerOverworld.gd
@@ -5,44 +5,44 @@ const initial_pos = Vector2(340, 360)
 var ismoving:bool = 0
 onready var PlayAnim = get_node("AnimationPlayer")
 
+var ActNorth:String = "north"
+var ActSouth:String = "south"
+var ActWest:String = "west"
+var ActEast:String = "east"
+
 func _ready():
 	PlayAnim.play("PIdle")
 
 func _physics_process(_delta):
 	var motion = Vector2()
-	if Input.is_action_pressed("north"):
-		#get_node("AnimationPlayer").play("PNorth")
+	if Input.is_action_pressed(ActNorth):
 		motion += Vector2(0, -1)
-		ismoving = 1
-	if Input.is_action_pressed("south"):
-		#get_node("AnimationPlayer").play("PSouth")
+		PlayerVars.ismoving = 1
+	if Input.is_action_pressed(ActSouth):
 		motion += Vector2(0, 1)
-		ismoving = 1
-	if Input.is_action_pressed("west"):
-		#get_node("AnimationPlayer").play("PWest")
+		PlayerVars.ismoving = 2
+	if Input.is_action_pressed(ActWest):
 		motion += Vector2(-1, 0)
-		ismoving = 1
-	if Input.is_action_pressed("east"):
-		#get_node("AnimationPlayer").play("PEast")
+		PlayerVars.ismoving = 3
+	if Input.is_action_pressed(ActEast):
 		motion += Vector2(1, 0)
-		ismoving = 1
+		PlayerVars.ismoving = 4
 	
-	if (ismoving):
-		if((abs(motion.x) < abs(motion.y)) && (motion.y <= 0)):
-			PlayAnim.play("PNorth")
-		if((abs(motion.x) < abs(motion.y)) && (motion.y > 0)):
-			PlayAnim.play("PSouth")
-		if((abs(motion.x) >= abs(motion.y)) && (motion.x >= 0)):
-			PlayAnim.play("PEast")
-		if((abs(motion.x) >= abs(motion.y)) && (motion.x < 0)):
-			PlayAnim.play("PWest")
+	if (PlayerVars.ismoving):
+		if(abs(motion.x) < abs(motion.y)):
+			if (motion.y <= 0):
+				PlayerVars.ismoving = 1
+			else:
+				PlayerVars.ismoving = 2
+		else:
+			if(motion.x >= 0):
+				PlayerVars.ismoving = 3
+			else:
+				PlayerVars.ismoving = 4
 	
-	
-	if Input.is_action_just_released("north") || Input.is_action_just_released("south") \
-		|| Input.is_action_just_released("west") || Input.is_action_just_released("east"):
-		
-		PlayAnim.play("PIdle")
-		ismoving = 0
+	if Input.is_action_just_released(ActNorth) || Input.is_action_just_released(ActSouth) \
+		|| Input.is_action_just_released(ActWest) || Input.is_action_just_released(ActEast):
+		PlayerVars.ismoving = 0
 		
 	motion = motion.normalized() * MOTION_SPEED
 
