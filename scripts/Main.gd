fe3c95ea06caac1580108082ffe01ef262f72a30
Hayden <hcoffey1@vols.utk.edu>
2019-11-15 16:23:04 -0500

    [dev/hc/main] Cleared commented out code

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,27 +1,10 @@
 extends Node2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-var player = preload("res://scenes/PlayerOverworld.tscn")
+#var player = preload("res://scenes/PlayerOverworld.tscn")
 onready var POWC = find_node("PlayerOverworld",true,false)
 
-# Called when the node enters the scene tree for the first time.
-#func _ready():
-#	create_player()
-		
-#func create_player():
-#	var new_player = player.instance()
-#	new_player.position = Vector2(100,30) #position + Vector2(20,1)
-#	add_child(new_player)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 func unpause():
 	POWC.get_node("POWCamera").make_current()
 
 func pause():
-	#POWC.get_node("POWCamera").current = false
-	#print(POWC.get_node("POWCamera").current)
 	get_tree().paused = true
